8ec498d2d482650da52b42d107862150bfaf3df6
Ani <intrepidani05@gmail.com>
2025-09-01 16:08:15 +0530

    refactor: update MongoDB connection logic and improve logging
    - Simplified the MongoDB connection URI construction by directly using MONGODB_URL from environment variables.
    - Enhanced logging to provide clearer connection status and database details.
    - Updated dependencies in pnpm-lock.yaml to their latest versions, including axios, dotenv, mongoose, and svix.
    - Removed deprecated node-fetch and updated nodemon to the latest version.
    - Ensured compatibility with the latest versions of various packages.

--- server/config/connectDB.config.js
+++ server/config/connectDB.config.js
@@ -2,24 +2,18 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-dotenv.config({
-  path: "./.env",
-});
+const DB_URI = `${process.env.MONGODB_URL}/${DB_NAME}`;
 
-const DB_URI = process.env.MONGO_ATLAS_URI;
-
-if (!DB_URI) {
-  console.log("URI IS MISSING! (From .env file)", DB_URI);
-}
+console.log(DB_URI);
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${DB_URI}/${DB_NAME}`);
-    console.log(
-      `\n MongoDB connected !! DB HOST ${
-        (connectionInstance.Connection.name, null, 2)
-      }`
-    );
+    const databaseResponse = await mongoose.connect(DB_URI);
+    console.log("MongoDB connected");
+    console.log({
+      "Host: --->": databaseResponse.connection.host,
+      "\nDatabase Name: --->": databaseResponse.connection.name,
+    });
   } catch (error) {
     console.log("MONGODB connection FAILED ", error.message);
     process.exit(1);
